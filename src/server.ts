01c1a8936b935f9bb3ccd83d1dfd48ce3fafba7e
banlon-jones <banlonjones@gmail.com>
2020-01-17 12:56:59 +0100

    stable version 2

--- src/server.ts
+++ src/server.ts
@@ -28,17 +28,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
     // deletes any files on the server on finish of the response
     {deleteLocalFiles([image])});
   });
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  
 
   /**************************************************************************** */
 
